97087108a94838681a2d6f30a2a4b0c0371e467f
tstoon3 <tstoon3@hotmail.com>
2025-04-24 15:47:36 +0700

    Update 24_4_25

--- src/components/LocationCo.vue
+++ src/components/LocationCo.vue
@@ -1,13 +1,34 @@
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
+
+const address = ref('');
+const coordinates = ref(null);
+
+const getCoordinates = async () =>{
+  try{
+    const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+      params:{
+        address:address.value,
+        key:'AIzaSyBnHYFLe-0JVKOSTX5_LYQg1UMFP-rlwhQ',
+      }
+    })
+    const location = response.data.result[0]?.geometry.location
+    coordinates.value = location
+  } catch(error){
+    console.error('ERROR',error)
+  }
+}
 
 const branches = ['Siam Paragon', 'Emsphere', 'Central World', 'Langsuan']
 
 // ค่าที่ถูกเลือก
 const selectedBranch = ref('')
+
 </script>
 <template>
   <div class="flex flex-col items-center">
+    
     <!-- แถวบน: รูป 3 ใบ -->
      
     <div class="grid grid-cols-3 w-full">
@@ -17,6 +38,8 @@ const selectedBranch = ref('')
       <img src="@/Img/WelcomeCTW.jpg" class="w-full h-70 object-cover blur-xs" />
       
     </div>
+    <!-- แถวล่าง: 1 รูปกลาง -->
+    <img src="@/Img/WelcomeRS.jpg" class="w-full h-70 object-cover mt-100 blur-xs" />
     <div class="absolute h-full w-full p-24">
         <div class="bg-white rounded-lg shadow-md overflow-hidden w-75 p-4">
             Location branch
@@ -30,15 +53,33 @@ const selectedBranch = ref('')
               </option>
             </select>
 
-            <p class="mt-4 text-sm text-gray-600">
+            <p class="mt-4 text-sm text-gray-600 pt-2">
               สาขาที่คุณเลือกคือ: <span class="font-semibold text-blue-600">{{ selectedBranch }}</span>
             </p>
           </div>
         </div>
       </div>
+      <div class="p-4 max-w-lg mx-auto space-y-4">
+    <input
+      type="text"
+      v-model="address"
+      placeholder="ป้อนที่อยู่ เช่น เซ็นทรัลเวิลด์ กรุงเทพ"
+      class="border px-4 py-2 rounded w-full"
+    />
+    <button
+      @click="getCoordinates"
+      class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+    >
+      แปลงเป็นพิกัด
+    </button>
 
-    <!-- แถวล่าง: 1 รูปกลาง -->
-    <img src="@/Img/WelcomeRS.jpg" class="w-full h-70 object-cover mt-100 blur-xs" />
+    <div v-if="coordinates" class="mt-4 text-sm text-gray-800">
+      📍 พิกัดที่ได้:  
+      <div>Lat: {{ coordinates.lat }}</div>
+      <div>Lng: {{ coordinates.lng }}</div>
+    </div>
+  </div>
+    
   </div>
 
 
--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 //import router from './router'
+//import VueGoogleMaps from '@fawmi/vue-google-maps'
 
 const app = createApp(App)
-
+/*app.use(VueGoogleMaps,{
+    load:{
+        key:'AIzaSyBnHYFLe-0JVKOSTX5_LYQg1UMFP-rlwhQ'
+    }
+})*/
 //app.use(router)
 
 app.mount('#app')
